417a059a97e49b6157d17e3772fd0137e44bc2a9
Jay-yee <54704544+Jay-yee@users.noreply.github.com>
2021-06-08 14:14:41 +0800

    Three: It could connect server!

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -3,6 +3,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.net.*;
 
 public class ChatClient extends Frame {
 
@@ -27,6 +29,7 @@ public class ChatClient extends Frame {
         });
         tftxt.addActionListener(new TfListener());
         setVisible(true);
+        connect();
     }
     //监听内部类
     private class TfListener implements ActionListener{
@@ -37,5 +40,15 @@ public class ChatClient extends Frame {
             taContent.setText(s);
             tftxt.setText(""); //让每次发送完信息后，输入框里面为空
         }
+    }
+
+    public void connect() {
+        try {
+            Socket s = new Socket("127.0.0.1",8888);
+            System.out.println("connected!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
