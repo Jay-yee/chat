4b3a0237a0351ad5ad3180a670e621d1bc386c99
Jay-yee <54704544+Jay-yee@users.noreply.github.com>
2021-06-09 13:59:09 +0800

    Eight: Add some new function about server can connect with many client

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,51 +1,82 @@
 import java.io.*;
 import java.net.*;
+import java.security.PrivateKey;
 
-public class ChatServer {
+public class ChatServer{
+    private boolean started = false;
+    private ServerSocket ss = null;
 
     public static void main(String[] args) {
+        new ChatServer().started();
+    }
 
-        boolean started = false;
-        ServerSocket ss = null;
-        Socket s = null;
-        DataInputStream dis = null;
-
-
+    public void started() {
         try {
             ss = new ServerSocket(8888);
             started = true;
-        }catch(BindException erro){
+        } catch (BindException erro) {
             System.out.println("This port has been used!");
-        }
-        catch (IOException e) {
+            System.out.println("Please close the application,and run again!");
+            System.exit(0);
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
-        try {
-            while (started) {
-                boolean connected = false;
-                s = ss.accept();
-                connected = true;
-                dis = new DataInputStream(s.getInputStream());
-                while (connected) {
-                    String str = dis.readUTF();
-                    System.out.println(str);
+            try {
+                while (started) {
+                    Socket s = ss.accept();
+                    System.out.println("A client connetcted!");
+                    new Thread(new Client(s)).start();
                 }
             }
-        }catch(EOFException e1){
-            System.out.println("Client closed!");
+             catch (IOException ioException) {
+                ioException.printStackTrace();
+            }finally {
+                try {
+                    ss.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-        catch ( IOException e) {
-            e.printStackTrace();
-        }
-        finally {
+    }
+
+    class Client implements Runnable{
+        Socket s = null;
+        DataInputStream dis = null;
+        private boolean bConnected = false;
+
+        public Client(Socket s) {
+            this.s = s;
             try {
-                if (dis != null) dis.close();
-                if (s != null) s.close();
+                dis = new DataInputStream(this.s.getInputStream());
+                bConnected = true;
             } catch (IOException ioException) {
                 ioException.printStackTrace();
             }
         }
 
+        @Override
+        public void run() {
+            try {
+                while (bConnected) {
+                    String str = dis.readUTF();
+                    System.out.println(str);
+                    if (str == "bye"){
+                        bConnected = false;
+                    }
+                }
+            }catch (EOFException e){
+                System.out.println("对方已经下线");}
+            catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+            finally {
+                try {
+                    if (dis != null) dis.close();
+                    if (s != null) s.close();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+            }
+        }
     }
-}
